046df2a4b80b5f65645488625f893b5e76fd8500
jsmnglng <jsmn.glng@gmail.com>
2019-04-12 13:18:07 -0700

    🚧 Still under construction

--- src/App/App.js
+++ src/App/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <AppContainer>
-        {/* <UnderConstruction /> */}
+        <UnderConstruction />
         <MainPage />
       </AppContainer>
     );
