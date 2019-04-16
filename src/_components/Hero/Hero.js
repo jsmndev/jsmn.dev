45f22effab6f4772bd1c82de606fbf4d9e696c16
jsmnglng <jsmn.glng@gmail.com>
2019-04-15 21:51:33 -0700

    Down arrow smooth scroll

--- src/_components/Hero/Hero.js
+++ src/_components/Hero/Hero.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link, animateScroll as scroll } from "react-scroll";
 import { HeroContainer } from "./Hero.style";
 
 const Hero = () => {
@@ -11,12 +12,19 @@ const Hero = () => {
         />
       </div>
       <div className="drop-down-icon">
-        <a href="#about">
+        <Link
+          activeClass="active"
+          to="about"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+        >
           <img
             src={require("../../_assets/img/down-arrow.png")}
             alt="down arrow"
           />
-        </a>
+        </Link>
       </div>
     </HeroContainer>
   );
